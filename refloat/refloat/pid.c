94c24383d1a66f344c93c7ddce299cf7742b668b
Aeraglyx <vladislav.cam24@gmail.com>
2024-05-11 05:00:38 +0200

    Experimental PID changes

--- refloat/refloat/pid.c
+++ refloat/refloat/pid.c
@@ -20,6 +20,7 @@
 #include "utils.h"
 
 #include <math.h>
+#include <stdint.h>
 
 void pid_reset(PID *pid) {
     pid->pid_value = 0.0f;
@@ -41,41 +42,54 @@ void pid_reset(PID *pid) {
 void pid_configure(PID *pid, const RefloatConfig *cfg) {
     pid->pitch_rate_alpha = half_time_to_alpha(cfg->kp2_filter, cfg->hertz);
     pid->ki = cfg->ki / cfg->hertz;
-    pid->softstart_ramp_step_size = 200.0f / cfg->hertz;
+    // pid->softstart_ramp_step_size = 200.0f / cfg->hertz;
+    const float softstart_ramp = 0.2f;  // in seconds
+    pid->softstart_ramp_step_size = 1.0f / (softstart_ramp * cfg->hertz);
 }
 
 void pid_update(PID *pid, const IMUData *imu, const MotorData *mot, const RefloatConfig *cfg, float setpoint) {
     // Prepare Brake Scaling (ramp scale values as needed for smooth transitions)
-    if (mot->erpm_abs < 500) {
-        // All scaling should roll back to 1.0x when near a stop for a smooth stand-still
-        // and back-forth transition
-        pid->kp_brake_scale = 0.01f + 0.99f * pid->kp_brake_scale;
-        pid->kp2_brake_scale = 0.01f + 0.99f * pid->kp2_brake_scale;
-        pid->kp_accel_scale = 0.01f + 0.99f * pid->kp_accel_scale;
-        pid->kp2_accel_scale = 0.01f + 0.99f * pid->kp2_accel_scale;
-    } else if (mot->erpm > 0) {
-        // Once rolling forward, brakes should transition to scaled values
-        pid->kp_brake_scale = 0.01f * cfg->kp_brake + 0.99f * pid->kp_brake_scale;
-        pid->kp2_brake_scale = 0.01f * cfg->kp2_brake + 0.99f * pid->kp2_brake_scale;
-        pid->kp_accel_scale = 0.01f + 0.99f * pid->kp_accel_scale;
-        pid->kp2_accel_scale = 0.01f + 0.99f * pid->kp2_accel_scale;
-    } else {
-        // Once rolling backward, the NEW brakes (we will use kp_accel) should transition to
-        // scaled values
-        pid->kp_brake_scale = 0.01f + 0.99f * pid->kp_brake_scale;
-        pid->kp2_brake_scale = 0.01f + 0.99f * pid->kp2_brake_scale;
-        pid->kp_accel_scale = 0.01f * cfg->kp_brake + 0.99f * pid->kp_accel_scale;
-        pid->kp2_accel_scale = 0.01f * cfg->kp2_brake + 0.99f * pid->kp2_accel_scale;
-    }
+    // if (mot->erpm_abs < 500) {
+    //     // All scaling should roll back to 1.0x when near a stop for a smooth stand-still
+    //     // and back-forth transition
+    //     pid->kp_brake_scale = 0.01f + 0.99f * pid->kp_brake_scale;
+    //     pid->kp2_brake_scale = 0.01f + 0.99f * pid->kp2_brake_scale;
+    //     pid->kp_accel_scale = 0.01f + 0.99f * pid->kp_accel_scale;
+    //     pid->kp2_accel_scale = 0.01f + 0.99f * pid->kp2_accel_scale;
+    // } else if (mot->erpm > 0) {
+    //     // Once rolling forward, brakes should transition to scaled values
+    //     pid->kp_brake_scale = 0.01f * cfg->kp_brake + 0.99f * pid->kp_brake_scale;
+    //     pid->kp2_brake_scale = 0.01f * cfg->kp2_brake + 0.99f * pid->kp2_brake_scale;
+    //     pid->kp_accel_scale = 0.01f + 0.99f * pid->kp_accel_scale;
+    //     pid->kp2_accel_scale = 0.01f + 0.99f * pid->kp2_accel_scale;
+    // } else {
+    //     // Once rolling backward, the NEW brakes (we will use kp_accel) should transition to
+    //     // scaled values
+    //     pid->kp_brake_scale = 0.01f + 0.99f * pid->kp_brake_scale;
+    //     pid->kp2_brake_scale = 0.01f + 0.99f * pid->kp2_brake_scale;
+    //     pid->kp_accel_scale = 0.01f * cfg->kp_brake + 0.99f * pid->kp_accel_scale;
+    //     pid->kp2_accel_scale = 0.01f * cfg->kp2_brake + 0.99f * pid->kp2_accel_scale;
+    // }
+
+    const float use_brake_scale = clamp(fabsf(mot->erpm_smooth) * 0.001f, 0.0f, 1.0f);
+    const float kp_brake_scale = 1.0f + (cfg->kp_brake - 1.0f) * use_brake_scale;
+    const float kp2_brake_scale = 1.0f + (cfg->kp2_brake - 1.0f) * use_brake_scale;
 
     const float pitch_offset = setpoint - imu->pitch_balance;
+    const int8_t direction = sign(mot->erpm_smooth);
 
     // PROPORTIONAL
-    float kp_scale;
-    if (pitch_offset < 0.0f) {
-        kp_scale = cfg->kp * pid->kp_brake_scale;
-    } else {
-        kp_scale = cfg->kp * pid->kp_accel_scale;
+    // float kp_scale;
+    // if (pitch_offset < 0.0f) {
+    //     kp_scale = cfg->kp * pid->kp_brake_scale;
+    // } else {
+    //     kp_scale = cfg->kp * pid->kp_accel_scale;
+    // }
+    // pid->proportional = pitch_offset * kp_scale;
+
+    float kp_scale = cfg->kp;
+    if (sign(pitch_offset) != direction) {
+        kp_scale *= kp_brake_scale;
     }
     pid->proportional = pitch_offset * kp_scale;
 
@@ -91,30 +105,40 @@ void pid_update(PID *pid, const IMUData *imu, const MotorData *mot, const Refloa
 
     // RATE P
     filter_ema(&pid->pitch_rate_filtered, -imu->gyro[1], pid->pitch_rate_alpha); 
-    // const float rate_prop = -imu->gyro[1];
     const float rate_prop = pid->pitch_rate_filtered;
-    float rate_p_scale;
-    if (rate_prop < 0.0f) {
-        rate_p_scale = cfg->kp2 * pid->kp2_brake_scale;
-    } else {
-        rate_p_scale = cfg->kp2 * pid->kp2_accel_scale;
+    // const float rate_prop = -imu->gyro[1];
+
+    float kp2_scale = cfg->kp2;
+    if (sign(rate_prop) != direction) {
+        kp2_scale *= kp2_brake_scale;
     }
-    pid->rate_p = rate_prop * rate_p_scale;
+    pid->rate_p = rate_prop * kp2_scale;
+
+    // float rate_p_scale;
+    // if (rate_prop < 0.0f) {
+    //     rate_p_scale = cfg->kp2 * pid->kp2_brake_scale;
+    // } else {
+    //     rate_p_scale = cfg->kp2 * pid->kp2_accel_scale;
+    // }
+    // pid->rate_p = rate_prop * rate_p_scale;
 
     // AGGREGATE
     float new_pid_value = pid->proportional + pid->integral + pid->rate_p;
 
     // SOFT START
-    if (pid->softstart_pid_limit < mot->current_max) {
-        pid->rate_p = fminf(fabs(pid->rate_p), pid->softstart_pid_limit) * sign(pid->rate_p);
-        pid->softstart_pid_limit += pid->softstart_ramp_step_size;
+    if (pid->softstart_pid_limit < 1.0f) {
+        new_pid_value *= pid->softstart_pid_limit;
+        const float limit_new = pid->softstart_pid_limit + pid->softstart_ramp_step_size;
+        pid->softstart_pid_limit = clamp(limit_new, 0.0f, 1.0f);
     }
+    // if (pid->softstart_pid_limit < mot->current_max) {
+    //     pid->rate_p = fminf(fabs(pid->rate_p), pid->softstart_pid_limit) * sign(pid->rate_p);
+    //     pid->softstart_pid_limit += pid->softstart_ramp_step_size;
+    // }
 
     // CURRENT LIMITING
     float current_limit = mot->braking ? mot->current_min : mot->current_max;
-    if (fabsf(new_pid_value) > current_limit) {
-        new_pid_value = sign(new_pid_value) * current_limit;
-    }
+    new_pid_value = clamp_sym(new_pid_value, current_limit);
 
-    pid->pid_value = pid->pid_value * 0.8f + new_pid_value * 0.2f;
+    filter_ema(&pid->pid_value, new_pid_value, 0.2f); 
 }
