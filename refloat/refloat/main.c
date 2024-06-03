cd8c03b22ad05f05add0cf2b539d67a298f002c2
Aeraglyx <vladislav.cam24@gmail.com>
2024-06-03 03:53:07 +0000

    Update real time debug

--- refloat/refloat/main.c
+++ refloat/refloat/main.c
@@ -1274,9 +1274,9 @@ static void send_realtime_data(data *d) {
         buffer_append_float32_auto(buffer, d->input_tilt.interpolated, &ind);
 
         // DEBUG
-        buffer_append_float32_auto(buffer, d->pid.pid_value, &ind);
-        buffer_append_float32_auto(buffer, 1.0f / max(d->loop_time_measured, 0.0001f), &ind);
-        buffer_append_float32_auto(buffer, d->warnings.duty, &ind);
+        buffer_append_float32_auto(buffer, d->config.faults.switch_half_speed, &ind);
+        buffer_append_float32_auto(buffer, d->imu.accel_derotated[2], &ind);
+        buffer_append_float32_auto(buffer, d->pid.drop_mult, &ind);
     }
 
     if (d->state.charging) {
